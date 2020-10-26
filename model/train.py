742073c0462a93da21a46b1cd0f5a5dd13a30e39
neenerrh <59638308+neenerrh@users.noreply.github.com>
2020-10-26 14:27:17 +0800

    Update train.py

--- model/train.py
+++ model/train.py
@@ -379,9 +379,9 @@ def train_by_sampling(args):
     #one.v_load_data(args.v_graph_file,args.weighted,args.directed)
     #one.v_load_attr(args.v_attribute_file,args.method)  
   
-    print(args.uattr)
-    if args.uattr == True:
-        one.u_load_attr(args.u_attribute_file,args.method)   
+    #print(args.uattr)
+    #if args.uattr == True:
+    one.u_load_attr(args.u_attribute_file,args.method)   
     
     one.v_load_attr(args.v_attribute_file,args.method)
     vectors_u=one.u_embedding(args.method,args.dim,args.ABRW_topk,args.ABRW_beta,args.ABRW_beta_mode,args.ABRW_alpha,args.number_walks,args.walk_length,args.window_size,args.workers,args.save_emb,args.u_emb_file)
