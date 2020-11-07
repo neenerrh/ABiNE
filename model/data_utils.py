3ae48cb4e87a6add6ef2815f9de22fc5a4922469
neenerrh <59638308+neenerrh@users.noreply.github.com>
2020-11-07 14:59:58 +0800

    Update data_utils.py

--- model/data_utils.py
+++ model/data_utils.py
@@ -23,34 +23,57 @@ class DataUtils(object):
                     fw.write(user + "\t" + item + "\t" + rating + "\n")
                     line = fin.readline()
 
-    def split_data(self, percent):
+     def split_data(self, percent,loss_function):
         """
         split data
         :param percent:
         :return:
         """
         test_user,test_item,test_rate,rating = set(), set(), {},{}
-        with open(os.path.join(self.model_path, "ratings.dat"), "r") as fin, open(os.path.join(self.model_path, "ratings_train.dat"),"w") as ftrain, open(os.path.join(self.model_path,"ratings_test.dat"), "w") as ftest:
-            for line in fin.readlines():
-                user, item, rate = line.strip().split("\t")
-                if rating.get(user) is None:
-                    rating[user] = {}
-                rating[user][item] = rate
-            for u in rating.keys():
-                item_list = rating[u].keys()
-                sample_list = random.sample(item_list, int(len(item_list) * percent))
-                for item in item_list:
-                    if item in sample_list:
-                        ftrain.write(u + "\t" + item + "\t" + rating[u][item] + "\n")
-                    else:
-                        if test_rate.get(u) is None:
-                            test_rate[u] = {}
-                        test_rate[u][item] = float(rating[u][item])
-                        test_user.add(u)
-                        test_item.add(item)
-                        ftest.write(u + "\t" + item + "\t" + rating[u][item] + "\n")
+        print(loss_function)
+        print(self.model_path)
+        
+        if loss_function == 0:
+            with open(os.path.join(self.model_path, "ratings1.dat"), "r") as fin, open(os.path.join(self.model_path, "ratings_train.dat"),"w") as ftrain, open(os.path.join(self.model_path,"ratings_test.dat"), "w") as ftest:
+                for line in fin.readlines():
+                    user, item, rate = line.strip().split("\t")
+                    if rating.get(user) is None:
+                        rating[user] = {}
+                    rating[user][item] = rate
+                for u in rating.keys():
+                    item_list = rating[u].keys()
+                    sample_list = random.sample(item_list, int(len(item_list) * percent))
+                    for item in item_list:
+                        if item in sample_list:
+                            ftrain.write(u + "\t" + item + "\t" + rating[u][item] + "\n")
+                        else:
+                            if test_rate.get(u) is None:
+                                test_rate[u] = {}
+                            test_rate[u][item] = float(rating[u][item])
+                            test_user.add(u)
+                            test_item.add(item)
+                            ftest.write(u + "\t" + item + "\t" + rating[u][item] + "\n")
+        else:
+            with open(os.path.join(self.model_path, "ratings.dat"), "r") as fin, open(os.path.join(self.model_path, "ratings_train.dat"),"w") as ftrain, open(os.path.join(self.model_path,"ratings_test.dat"), "w") as ftest:
+                for line in fin.readlines():
+                    user, item, rate = line.strip().split("\t")
+                    if rating.get(user) is None:
+                        rating[user] = {}
+                    rating[user][item] = rate
+                for u in rating.keys():
+                    item_list = rating[u].keys()
+                    sample_list = random.sample(item_list, int(len(item_list) * percent))
+                    for item in item_list:
+                        if item in sample_list:
+                            ftrain.write(u + "\t" + item + "\t" + rating[u][item] + "\n")
+                        else:
+                            if test_rate.get(u) is None:
+                                test_rate[u] = {}
+                            test_rate[u][item] = float(rating[u][item])
+                            test_user.add(u)
+                            test_item.add(item)
+                            ftest.write(u + "\t" + item + "\t" + rating[u][item] + "\n")
         return test_user, test_item, test_rate
-
     
     def read_train_data(self,filename=None):
         if filename is None:
