99a6516cf50fba530a6ef37ef33a6b52168c6560
neenerrh <59638308+neenerrh@users.noreply.github.com>
2020-11-09 20:12:36 +0800

    Update train.py

--- model/train.py
+++ model/train.py
@@ -391,12 +391,12 @@ def train_by_sampling(args):
     one.u_load_attr(args.u_attribute_file,args.method)   
     
     one.v_load_attr(args.v_attribute_file,args.method)
-    vectors_u=one.u_embedding(args.method,args.dim,args.ABRW_topk,args.ABRW_beta,args.ABRW_beta_mode,args.ABRW_alpha,args.number_walks,args.walk_length,args.window_size,args.workers,args.save_emb,args.u_emb_file)
+    vectors_u,node_u_num=one.u_embedding(args.method,args.dim,args.ABRW_topk,args.ABRW_beta,args.ABRW_beta_mode,args.ABRW_alpha,args.number_walks,args.walk_length,args.window_size,args.workers,args.save_emb,args.u_emb_file)
   
    
     
 
-    vectors_v=one.v_embedding(args.method,args.dim,args.ABRW_topk,args.ABRW_beta,args.ABRW_beta_mode,args.ABRW_alpha,args.number_walks,args.walk_length,args.window_size,args.workers,args.save_emb,args.v_emb_file)
+    vectors_v,node_v_num=one.v_embedding(args.method,args.dim,args.ABRW_topk,args.ABRW_beta,args.ABRW_beta_mode,args.ABRW_alpha,args.number_walks,args.walk_length,args.window_size,args.workers,args.save_emb,args.v_emb_file)
 
     
   
@@ -406,7 +406,7 @@ def train_by_sampling(args):
         pointwise(vectors_u,vectors_v, edge_list, edge_dict_u,args.max_iter,alpha, beta, gamma, lam)
         
     else :
-        bpr=BPR(model_path,vectors_u,vectors_v,args.dim,n_train,train_user,train_item)  
+        bpr=BPR(model_path,node_u_num,node_v_num,vectors_u,vectors_v,args.dim,n_train,train_user,train_item)  
         vectors_u,vectors_v=bpr.fit()
         
      
