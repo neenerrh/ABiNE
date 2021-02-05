06312bbf2e08a933640a75dca8542870950bc370
neenerrh <59638308+neenerrh@users.noreply.github.com>
2021-02-05 15:28:01 +0800

    Update grid2.sh

--- bash/grid2.sh
+++ bash/grid2.sh
@@ -7,51 +7,51 @@ echo '......ABiNENegbatch bash......'
 echo '------------ ABiNENegbatch.txt-----------------------------------------'
 echo '------------ABiNENegbatch.txt----------------------------------------' > bash/log/ABiNENegbatch.txt
 start=`date +%s`
-python model/train.py --train-data data/mooc/months1/sample1/ratings_train.csv --test-data data/mooc/months1/sample1/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 20 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
+python model/train.py --train-data data/mooc/months1/sample1/ratings_train.csv --test-data data/mooc/months1/sample1/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 10 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
 end=`date +%s`
 echo ALL running time: $((end-start)) >> bash/log/ABiNENegbatch.txt
 
 start=`date +%s`
-python model/train.py --train-data data/mooc/months1/sample2/ratings_train.csv --test-data data/mooc/months1/sample2/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 20 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
+python model/train.py --train-data data/mooc/months1/sample2/ratings_train.csv --test-data data/mooc/months1/sample2/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 10 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
 end=`date +%s`
 echo ALL running time: $((end-start)) >> bash/log/ABiNENegbatch.txt
 
 start=`date +%s`
-python model/train.py --train-data data/mooc/months1/sample3/ratings_train.csv --test-data data/mooc/months1/sample3/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 20 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
+python model/train.py --train-data data/mooc/months1/sample3/ratings_train.csv --test-data data/mooc/months1/sample3/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 10 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
 end=`date +%s`
 echo ALL running time: $((end-start)) >> bash/log/ABiNENegbatch.txt
 
 start=`date +%s`
-python model/train.py --train-data data/mooc/months1/sample4/ratings_train.csv --test-data data/mooc/months1/sample4/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 20 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
+python model/train.py --train-data data/mooc/months1/sample4/ratings_train.csv --test-data data/mooc/months1/sample4/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 10 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
 end=`date +%s`
 echo ALL running time: $((end-start)) >> bash/log/ABiNENegbatch.txt
 
 start=`date +%s`
-python model/train.py --train-data data/mooc/months1/sample5/ratings_train.csv --test-data data/mooc/months1/sample5/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 20 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
+python model/train.py --train-data data/mooc/months1/sample5/ratings_train.csv --test-data data/mooc/months1/sample5/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 10 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
 end=`date +%s`
 echo ALL running time: $((end-start)) >> bash/log/ABiNENegbatch.txt
 
 start=`date +%s`
-python model/train.py --train-data data/mooc/months1/sample6/ratings_train.csv --test-data data/mooc/months1/sample6/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 20 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
+python model/train.py --train-data data/mooc/months1/sample6/ratings_train.csv --test-data data/mooc/months1/sample6/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 10 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
 end=`date +%s`
 echo ALL running time: $((end-start)) >> bash/log/ABiNENegbatch.txt
 
 start=`date +%s`
-python model/train.py --train-data data/mooc/months1/sample7/ratings_train.csv --test-data data/mooc/months1/sample7/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 20 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
+python model/train.py --train-data data/mooc/months1/sample7/ratings_train.csv --test-data data/mooc/months1/sample7/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 10 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
 end=`date +%s`
 echo ALL running time: $((end-start)) >> bash/log/ABiNENegbatch.txt
 
 start=`date +%s`
-python model/train.py --train-data data/mooc/months1/sample8/ratings_train.csv --test-data data/mooc/months1/sample8/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 20 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
+python model/train.py --train-data data/mooc/months1/sample8/ratings_train.csv --test-data data/mooc/months1/sample8/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 10 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
 end=`date +%s`
 echo ALL running time: $((end-start)) >> bash/log/ABiNENegbatch.txt
 
 start=`date +%s`
-python model/train.py --train-data data/mooc/months1/sample9/ratings_train.csv --test-data data/mooc/months1/sample9/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 20 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
+python model/train.py --train-data data/mooc/months1/sample9/ratings_train.csv --test-data data/mooc/months1/sample9/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 10 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
 end=`date +%s`
 echo ALL running time: $((end-start)) >> bash/log/ABiNENegbatch.txt
 
 start=`date +%s`
-python model/train.py --train-data data/mooc/months1/sample10/ratings_train.csv --test-data data/mooc/months1/sample10/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 20 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
+python model/train.py --train-data data/mooc/months1/sample10/ratings_train.csv --test-data data/mooc/months1/sample10/ratings_test.csv --alpha 0.01 --beta 0.01 --gamma 0.1 --lam 0.025 --lr 0.01 --ws 5 --ns 4 --walk-length 80 --number-walks 3 --max-iter 50 --d 128 --top-n 10 --ABRW-topk 30 --ABRW-beta 0.2 >> bash/log/ABiNENegbatch.txt
 end=`date +%s`
 echo ALL running time: $((end-start)) >> bash/log/ABiNENegbatch.txt
